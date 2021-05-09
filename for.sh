33b411c1a3295df36b42289d60fd3949c08f7fb7
netdioxadmini <netdioxadmini@gmail.com>
2021-05-09 20:03:07 +0530

    comment added

--- for.sh
+++ for.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash 
-
+# brace expansion 
 for i in {1..100}
 do
     echo "$i"
 done
 
-# Another Version
+# Another Version Arithemetic Evaluation 
 
 for (( n=100;n<=200;n++ ))
 do
@@ -19,4 +19,11 @@ declare -a fruits=("apple" "banana" "cherry")
 for i in ${fruits[@]}
 do
     echo "Today we have $i"
+done
+
+# Command Substitution
+
+for d in $(ls)
+do 
+echo "$d"
 done
