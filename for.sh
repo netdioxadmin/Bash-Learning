aa0124c8f9644410141bad25d051ddb25a59813d
netdioxadmini <netdioxadmini@gmail.com>
2021-05-09 19:57:22 +0530

    paramater expansion

--- for.sh
+++ for.sh
@@ -11,3 +11,12 @@ for (( n=100;n<=200;n++ ))
 do
     echo $n
 done
+
+# Expanding With Paramater expansion 
+
+declare -a fruits=("apple" "banana" "cherry")
+
+for i in ${fruits[@]}
+do
+    echo "Today we have $i"
+done
